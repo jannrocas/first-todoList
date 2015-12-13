c3ba5c245ebe2ee6fc70b95df9b5098038c92c55
Cyrxe Iliad <jannrocas@gmail.com>
2015-12-13 14:02:44 +0800

    first commit

---
+++ webpack/components/CreateLabel.js
@@ -0,0 +1,40 @@
+import React, { Component, PropTypes } from 'react'
+var ImmutableRenderMixin = require('react-immutable-render-mixin');
+
+
+import './Todo.scss';
+
+var CreateLabel = React.createClass({
+	displayName: 'AddLabels',
+
+  mixins: [ImmutableRenderMixin],
+
+  getInitialState() {
+    return {};
+  },
+  
+  handleCreateLabel(event) {
+    event.preventDefault();
+    let input = this.refs.label_text;
+
+    if (input.value.length > 0) {
+      this.props.createLabel(input.value);
+      input.value = '';
+    }
+  },
+  
+  render() {
+    return (
+    	<span style={{display:'inline-block', position:'relative'}}>
+			<form onSubmit={this.handleCreateLabel}>
+          <input ref="label_text" type="text" placeholder="Create new label"/>
+          <button type="submit">Create Label </button>
+        </form>
+      </span>
+    )
+  }
+	
+	
+});
+
+module.exports = CreateLabel;
